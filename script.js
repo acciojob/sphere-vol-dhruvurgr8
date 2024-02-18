45fe9ed39ecdd22bd77e6f8408187870b7c84bab
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-18 17:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+// script.js
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+function volume_sphere() {
+ 
+  var radiusInput = document.getElementById("radius").value;
+
+  
+  var radius = parseFloat(radiusInput);
+
+ 
+  if (isNaN(radius) || radius < 0) {
+    
+    document.getElementById("volume").value = 'NaN';
+  } else {
+    
+    var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    
+    volume = volume.toFixed(4);
+
+    
+    document.getElementById("volume").value = volume;
+  }
+}
+
+
+document.getElementById("MyForm").addEventListener("submit", function (event) {
+  event.preventDefault(); 
+  volume_sphere(); 
+});
